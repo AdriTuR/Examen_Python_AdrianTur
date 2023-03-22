42bb3ed9a20d588e0c009b72598a45ca38a50603
AdriTuR <74455555+AdriTuR@users.noreply.github.com>
2023-03-22 11:13:02 +0100

    commit_4

--- functions.py
+++ functions.py
@@ -2,49 +2,65 @@ import csv
 #===========================================
 #============= READ DATA() =================
 
-
-
-
-def read_data(fichero):
-    data = {}
-    with open(fichero, "r") as f:
+def read_data(fichero_csv):
+    datos = {}
+    with open(fichero_csv, "r") as f:
         reader = csv.reader(f)
         atributos = next(reader)
         for i, row in enumerate(reader):
             if row[0] != "":
-                data["dato"+str(i+1)] = {}
+                datos["dato"+str(i+1)] = {}
                 for j, atributo in enumerate(atributos):
-                    data["dato"+str(i+1)][atributo] = row[j]
-    return data
+                    datos["dato"+str(i+1)][atributo] = row[j]
+    return datos
 
+#===========================================
+#=============== SLPLIT() ==================
 
-def split(data, atributo):
-    white = {}
-    red = {}
-    for i in data:
-        if atributo in data[i]:
-            if data[i][atributo] == "white":
-                white[i] = data[i]
-                del white[i][atributo]
-            elif data[i][atributo] == "red":
-                red[i] = data[i]
-                del red[i][atributo]
+def split(dicionario, atributo):
+    diccionario_white = {}
+    diccionario_red = {}
+    for i in dicionario:
+        if atributo in dicionario[i]:
+            if dicionario[i][atributo] == "white":
+                diccionario_white[i] = dicionario[i]
+                del diccionario_white[i][atributo]
+            elif dicionario[i][atributo] == "red":
+                diccionario_red[i] = dicionario[i]
+                del diccionario_red[i][atributo]
             else:
-                raise ValueError("Este atributo no existe")
+                raise ValueError("El atributo seleccionado no existe")
         else:
-            raise ValueError("Este atributo no existe")
-    return white, red
+            raise ValueError("El atributo seleccionado no existe")
+    return diccionario_white, diccionario_red
 
+#===========================================
+#=============== REDUCE() ==================
 
-def reduce(data, atributo):
-    lista = []
-    for i in data:
-        if atributo in data[i]:
-            lista.append(data[i][atributo])
+def reduce(diccionario, atributo):
+    lista_reduce = []
+    for i in diccionario:
+        if atributo in diccionario[i]:
+            lista_reduce.append(diccionario[i][atributo])
         else:
-            raise ValueError("El atributo no existe")
-    return lista
+            raise ValueError("El atributo seleccionado no existe")
+    return lista_reduce
 
+#===========================================
+#============= SILHOUETTE() ================
+
+def silhouette(lista1, lista2):
+    a = 0
+    b = 0
+    for i in lista1:
+        for j in lista1:
+            if i != j:
+                a += (abs(float(i) - float(j)))**2
+        a = a / (len(lista1) - 1)
+        for k in lista2:
+            b += (abs(float(i) - float(k)))**2
+        b = b / len(lista2)
+        
 
 
 
--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+#===========================================
+#================== IMPORTS ================
 import functions
+#===========================================
+#================= MAIN ===================
 
-data = functions.read_data("winequality.csv")
-print(data)
+#read_data()
+ruta = ".\Examen_Python_AdrianTur\winequality.csv"
+data = functions.read_data(ruta)
 
+#split()
+dictwhite = {}
+dictred = {}
+dictwhite, dictred = functions.split(data, "type")
+
+#reduce()
+lista_alcohol = []
+lista_density = []
+lista_alcohol = functions.reduce(data, "alcohol")
+lista_density = functions.reduce(data, "density")
