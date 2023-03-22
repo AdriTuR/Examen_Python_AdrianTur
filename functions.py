5b3247d73e9bf980103239b72e38f7d76651f403
AdriTuR <74455555+AdriTuR@users.noreply.github.com>
2023-03-22 10:46:19 +0100

    commit_3

--- functions.py
+++ functions.py
@@ -2,13 +2,39 @@ import csv
 #===========================================
 #============= READ DATA() =================
 
-def read_data(nombrearchivo):
+
+
+
+def read_data(fichero):
     data = {}
-    with open(nombrearchivo, newline='') as csvfile:
-        text = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in text:
-            data[row[0]] = row[1]
+    with open(fichero, "r") as f:
+        reader = csv.reader(f)
+        atributos = next(reader)
+        for i, row in enumerate(reader):
+            if row[0] != "":
+                data["dato"+str(i+1)] = {}
+                for j, atributo in enumerate(atributos):
+                    data["dato"+str(i+1)][atributo] = row[j]
     return data
+
+
+def split(data, atributo):
+    white = {}
+    red = {}
+    for i in data:
+        if atributo in data[i]:
+            if data[i][atributo] == "white":
+                white[i] = data[i]
+                del white[i][atributo]
+            elif data[i][atributo] == "red":
+                red[i] = data[i]
+                del red[i][atributo]
+            else:
+                raise ValueError("Este atributo no existe")
+        else:
+            raise ValueError("Este atributo no existe")
+    return white, red
+
 
 def reduce(data, atributo):
     lista = []
@@ -18,3 +44,8 @@ def reduce(data, atributo):
         else:
             raise ValueError("El atributo no existe")
     return lista
+
+
+
+
+
--- main.py
+++ main.py
@@ -2,3 +2,4 @@ import functions
 
 data = functions.read_data("winequality.csv")
 print(data)
+
