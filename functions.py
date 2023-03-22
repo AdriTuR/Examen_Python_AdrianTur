305b9914219ac8ffc841b13cb819a331ae92f533
AdriTuR <74455555+AdriTuR@users.noreply.github.com>
2023-03-22 10:30:42 +0100

    commit_2

--- functions.py
+++ functions.py
@@ -1,12 +1,20 @@
+import csv
 #===========================================
 #============= READ DATA() =================
 
-def read_data(filename):
+def read_data(nombrearchivo):
     data = {}
-    with open(filename, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in spamreader:
+    with open(nombrearchivo, newline='') as csvfile:
+        text = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in text:
             data[row[0]] = row[1]
     return data
 
-
+def reduce(data, atributo):
+    lista = []
+    for i in data:
+        if atributo in data[i]:
+            lista.append(data[i][atributo])
+        else:
+            raise ValueError("El atributo no existe")
+    return lista
---
+++ main.py
@@ -0,0 +1,4 @@
+import functions
+
+data = functions.read_data("winequality.csv")
+print(data)
